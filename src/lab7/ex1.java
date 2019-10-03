0cdbea90a2aecfd9d033f799c09ce9229f072fe0
supawann1999@gmail.com <Botba109@gmail.com>
2019-10-03 16:36:41 +0700

    lab7

---
+++ src/lab7/ex1.java
@@ -0,0 +1,24 @@
+package lab7;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        int x =100, y = 20;
+
+        //if
+        if (x < y && x==100 || y>10) {
+            //statements
+            System.out.println("x less than y.");
+        }else {
+            System.out.println("x more than y.");
+        }
+
+
+
+
+    }//main
+
+
+
+
+}//class
---
+++ src/lab7/ex2.java
@@ -0,0 +1,41 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your score, " + "and i will tell you grade:");
+
+        int score = sc.nextInt();
+
+        if (score > 80)
+            System.out.println("Grade A.");
+        else if (score > 75)
+            System.out.println("Grade B.");
+        else if (score > 70)
+            System.out.println("Grade B+.");
+        else if (score > 65)
+            System.out.println("Grade C.");
+        else if (score > 60)
+            System.out.println("Grade C+.");
+        else if (score > 55)
+            System.out.println("Grade D.");
+        else if (score > 50)
+            System.out.println("Grade D+.");
+        else
+            System.out.println("Grade F.");
+
+    }
+}
+
+
+
+
+
+
+
+
+
