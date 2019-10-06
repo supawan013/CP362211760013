9cb0af8e6fc611ebfff175f3f726e91bdb03be0f
supawann1999@gmail.com <Botba109@gmail.com>
2019-10-06 11:23:38 +0700

    lab7.1

---
+++ src/lab7/ex3.java
@@ -0,0 +1,28 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Which subjects you like the most? : ");
+        System.out.println("1. System Analysis and Design. : ");
+        System.out.println("2. Computer Programming. : ");
+        System.out.println("3. Internet Application Programming . : ");
+        System.out.print("Select : ");
+        int selector = sc.nextInt();
+
+        //switch-case
+        switch (selector){
+            case 1: System.out.println("Sure? This subject is very hard.");break;
+            case 2: System.out.println("Yer, Computer Programming is Very fun");break;
+            case 3: System.out.println("ohhh, you might expert in Programming ");break;
+            default: System.out.println("You have to enter 1-3.");
+        }
+
+
+
+    }
+}
---
+++ src/lab7/ex4.java
@@ -0,0 +1,16 @@
+package lab7;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10 ; i++) {
+            if (i %2 ==0) {
+                System.out.print("Hello.");
+                System.out.println("\tFILM");
+            }else
+                System.out.println("hello.");
+
+            }
+
+        }
+    }
