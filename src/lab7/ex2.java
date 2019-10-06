90f51afdfc889ea0debf0636383a92d891519d52
supawann1999@gmail.com <Botba109@gmail.com>
2019-10-06 15:05:45 +0700

    lab7.1

--- src/lab7/ex2.java
+++ src/lab7/ex2.java
@@ -6,29 +6,30 @@ public class ex2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int score;
+        do {
+            System.out.print("Enter your score, " + "and i will tell you grade:");
+            score = sc.nextInt();
 
-        System.out.print("Enter your score, " + "and i will tell you grade:");
-
-        int score = sc.nextInt();
-
-        if (score > 0 && score<49) // 0-49
+        if (score > 0 && score < 49) // 0-49
             System.out.println("Grade F.");
-        else if (score >= 50&& score<55) //50-54
+        else if (score >= 50 && score < 55) //50-54
+            System.out.println("Grade D+.");
+        else if (score >= 55 && score < 60) //50-54
             System.out.println("Grade D.");
-        else if (score >= 55&& score<60) //50-54
-            System.out.println("Grade D.");
-        else if (score >= 60&& score<65) //55-59
+        else if (score >= 60 && score < 65) //55-59
             System.out.println("Grade C+.");
-        else if (score >= 65&& score<70) //60-64
+        else if (score >= 65 && score < 70) //60-64
             System.out.println("Grade C.");
-        else if (score >= 70&& score<75) //65-69
+        else if (score >= 70 && score < 75) //65-69
             System.out.println("Grade B+.");
-        else if (score >= 75&& score<80) //75-79
+        else if (score >= 75 && score < 80) //75-79
             System.out.println("Grade B.");
-        else if (score >= 80&& score<100) //80-100
+        else if (score >= 80 && score < 100) //80-100
             System.out.println("Grade A.");
-
-            System.out.println("Please enter a score between0-100.");
+        else
+        System.out.println("Please enter a score between0-100.");
+    }while (!(score >=0 && score <=100));
 
     }
 }
---
+++ src/lab7/ex6.java
@@ -0,0 +1,20 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x=1;
+
+        while (x !=0) {
+            System.out.print("Enter an integer : ");
+            x = sc.nextInt();
+        }
+
+        System.out.println("Good Bye");
+
+    }//main
+}//class
